22e7740d021d40d979aedea6e0f112f0a5f67ba7
carina <rin.ramazanova@yandex.com>
2024-06-30 21:55:19 +0300

    DEPLOYED

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import '@hotwired/turbo-rails';
-import 'controllers';
-import './menubar';
-import './timer';
+//= require custom/timer
+//= require custom/menubar
+//= require custom/worktoggleswitch
+
 import '@hotwired/turbo-rails';
