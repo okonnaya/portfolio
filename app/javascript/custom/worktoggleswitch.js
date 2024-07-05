d242935782c2137cb1607282cc7b9840bc97bded
Aydar <me@aydar.media>
2024-07-06 00:58:11 +0300

    safer switch

--- app/javascript/custom/worktoggleswitch.js
+++ app/javascript/custom/worktoggleswitch.js
@@ -1,28 +1,30 @@
 console.log('ьлктмщукт');
 
-var checkbox = document.getElementById('e');
-var graphicElements = document.querySelectorAll('.W_WorkPicsGraphic');
-var webElements = document.querySelectorAll('.W_WorkPicsWeb');
+document.addEventListener('DOMContentLoaded', () => {
+  var checkbox = document.getElementById('e');
+  var graphicElements = document.querySelectorAll('.W_WorkPicsGraphic');
+  var webElements = document.querySelectorAll('.W_WorkPicsWeb');
 
-function updateDisplay() {
-  if (checkbox.checked) {
-    graphicElements.forEach(function (element) {
-      element.classList.remove('DisplayNone');
-    });
-    webElements.forEach(function (element) {
-      element.classList.add('DisplayNone');
-    });
-  } else {
-    graphicElements.forEach(function (element) {
-      element.classList.add('DisplayNone');
-    });
-    webElements.forEach(function (element) {
-      element.classList.remove('DisplayNone');
-    });
+  function updateDisplay() {
+    if (checkbox.checked) {
+      graphicElements.forEach(function (element) {
+        element.classList.remove('DisplayNone');
+      });
+      webElements.forEach(function (element) {
+        element.classList.add('DisplayNone');
+      });
+    } else {
+      graphicElements.forEach(function (element) {
+        element.classList.add('DisplayNone');
+      });
+      webElements.forEach(function (element) {
+        element.classList.remove('DisplayNone');
+      });
+    }
   }
-}
 
-checkbox.addEventListener('change', updateDisplay);
+  checkbox.addEventListener('change', updateDisplay);
 
-// Initialize the display based on the current state of the checkbox
-updateDisplay();
+  // Initialize the display based on the current state of the checkbox
+  updateDisplay();
+});
